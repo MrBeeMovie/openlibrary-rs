8bbba9ac552306acea7f72a163132f48bd2bca74
Max Wertheim <36082261+MrBeeMovie@users.noreply.github.com>
2022-07-21 18:30:07 -0400

    Goodreads search results (#1)
    * Added Query struct which can be used to get search results from Goodreads.
    
    * Results now have url in /book/show/id format.
    
    * Refactored file tree. Book struct now only stores ids. Made closure to replace ugly code in Query::get_books().
    
    * Removed uneeded lets in Query::get_books().

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
-fn main() {
-    println!("Hello, world!");
-}
+#[macro_use]
+extern crate enum_display_derive;
+
+fn main() {}
---
+++ src/search.rs
@@ -0,0 +1,4 @@
+pub mod book;
+pub mod query;
+
+const GOODREADS_URL: &str = "https://www.goodreads.com";
