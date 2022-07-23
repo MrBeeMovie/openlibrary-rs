20f8b2060ccaad34e692a7d33a2542cc80795c97
Max Wertheim <minecart321@gmail.com>
2022-07-23 11:18:03 -0400

    Added more checks to test.

--- src/search.rs
+++ src/search.rs
@@ -1,18 +1,20 @@
 pub mod book;
 
-use serde_derive::Deserialize;
+use serde_derive::{Deserialize, Serialize};
 
 const OPENLIBRARY_URL: &str = "https://openlibrary.org";
 
-#[derive(Deserialize, Debug)]
-#[serde(rename_all(deserialize = "camelCase"))]
+#[derive(Serialize, Deserialize, Default, Debug)]
+#[serde(rename_all(serialize = "camelCase", deserialize = "camelCase"))]
+#[serde(default)]
 pub struct SearchDoc {
     key: String,
     title: String,
 }
 
-#[derive(Deserialize, Debug)]
-#[serde(rename_all(deserialize = "camelCase"))]
+#[derive(Serialize, Deserialize, Default, Debug)]
+#[serde(rename_all(serialize = "camelCase", deserialize = "camelCase"))]
+#[serde(default)]
 pub struct SearchResult {
     num_found: u32,
     start: u32,
@@ -41,12 +43,74 @@ impl Search {
     pub fn execute(&self) -> SearchResult {
         let search_fields = self.search_fields.join(",");
 
-        let response = reqwest::blocking::get(format!(
+        #[cfg(not(test))]
+        let url = format!(
             "{}/search.json?q={}&fields={}",
             OPENLIBRARY_URL, self.search_term, search_fields
-        ))
-        .unwrap();
+        );
+        #[cfg(test)]
+        let url = format!(
+            "{}/search.json?q={}&fields={}",
+            mockito::server_url(),
+            self.search_term,
+            search_fields
+        );
+
+        let response = reqwest::blocking::get(url).unwrap();
 
         response.json().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use mockito::mock;
+    use serde_json::{json, Value};
+
+    use super::{Search, SearchResult};
+
+    fn get_search_result(search_term: &str, search_fields: Vec<&str>, json: Value) -> SearchResult {
+        let search = Search::new(search_term, search_fields.clone());
+        let search_fields = search_fields.join(",");
+
+        let _m = mock(
+            "GET",
+            format!("/search.json?q={}&fields={}", search_term, search_fields).as_str(),
+        )
+        .with_header("content-type", "application/json")
+        .with_body(json.to_string())
+        .create();
+
+        search.execute()
+    }
+
+    #[test]
+    fn test_search_execute_valid_response() {
+        let search_term = "test";
+        let search_fields = vec!["key", "title"];
+
+        let json = json!({
+                "numFound": 1,
+                "start": 0,
+                "numFoundExact": true,
+                "docs": [
+                    {
+                        "key": "/works/43242",
+                        "title": "test",
+                    }
+                ]
+        });
+
+        let search_result = get_search_result(search_term, search_fields, json);
+
+        assert_eq!(search_result.num_found, 1);
+        assert_eq!(search_result.start, 0);
+        assert_eq!(search_result.num_found_exact, true);
+        assert_eq!(search_result.docs.len(), 1);
+
+        let doc = &search_result.docs[0];
+
+        assert_eq!(doc.key, "/works/43242");
+        assert_eq!(doc.title, "test");
     }
 }
