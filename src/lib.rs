4d1b0c4d13aed0170d18c616b1f190a113ad4da8
Max Wertheim <minecart321@gmail.com>
2022-08-05 12:42:50 -0400

    Fixed example variable names and updated docs in lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,58 @@
 //! Wrapper for [Openlibrary's Web API](https://openlibrary.org/developers/api)
 //!
+//! # Overview
+//!
+//! **PLEASE NOTE**: this library is currently in an experimental stage.
+//! Meaning expect frequent, large breaking changes from version to version until we are in a stable state.
+//!
 //! There are currently 8 sub APIs offered by Openlibrary's Web API.
 //! This library is still in development and does not have a wrapper for each yet.
 //! You can check the table below to see the status of each.
 //!
-//! - [ ] Books
-//! - [ ] Authors
-//! - [ ] Subjects
-//! - [X] Search
-//! - [ ] Search inside
-//! - [ ] Partner
-//! - [ ] Covers
-//! - [ ] Recent Changes
+//! - [X] [Books](https://openlibrary.org/dev/docs/api/books) [^books_completeness]
+//! - [ ] [Authors](https://openlibrary.org/dev/docs/api/authors)
+//! - [ ] [Subjects](https://openlibrary.org/dev/docs/api/subjects)
+//! - [X] [Search](https://openlibrary.org/dev/docs/api/search)
+//! - [ ] [Search inside](https://openlibrary.org/dev/docs/api/search_inside)
+//! - [ ] [Partner](https://openlibrary.org/dev/docs/api/read)
+//! - [ ] [Covers](https://openlibrary.org/dev/docs/api/covers)
+//! - [ ] [Recent Changes](https://openlibrary.org/dev/docs/api/recentchanges)
+//!
+//! # Books
+//!
+//! You can view information about books by Works, Editions, and ISBN ids by using the [`books::Books`] struct
+//!
+//! ``` rust
+//! use openlibrary_rs::books::{BookType, BooksBuilder};
+//!
+//! // execute request to Works API and pretty print debug of result
+//! let books = BooksBuilder::default()
+//!     .book_type(BookType::Works)
+//!     .id("OL45883W")
+//!     .build()
+//!     .unwrap();
+//!
+//!     println!("{:#?}", books.execute());
+//! ```
 //!
 //! # Search
 //!
-//! You can search for books, authors, and more using the [`search::Search`] struct[^note]
+//! You can search for books, authors, and more using the [`search::Search`] struct
 //!
-//! Example
 //! ``` rust
 //! use openlibrary_rs::search::SearchBuilder;
 //!
 //! // execute search and pretty print debug of first result
-//! let results = SearchBuilder::default()
+//! let search = SearchBuilder::default()
 //!     .query("the lord of the rings")
 //!     .build()
 //!     .unwrap();
 //!
-//! println!("{:#?}", results.execute().docs[0]);
+//! println!("{:#?}", search.execute().docs[0]);
 //! ```
 //!
-//! [^note]: You must use the [`search::SearchBuilder`] to build instances of [`search::Search`] as all fields are private
+//! [^books_completeness]: Everything excluding the generic Books API is complete(i.e. Works, Editions, and ISBN APIs are done).
+//!
 
 #[allow(dead_code)]
 const OPENLIBRARY_URL: &str = "https://openlibrary.org";
